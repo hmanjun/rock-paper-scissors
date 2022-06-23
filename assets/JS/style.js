b1e1719835d8c77badfe7e446b9bf584bdd294b6
hmanjun <harshith.hm03@gmail.com>
2022-06-23 11:27:16 -0700

    Add user input prmopt

---
+++ assets/JS/style.js
@@ -0,0 +1,18 @@
+var gameOver = false
+var potnArr = ["r","p","s"]
+
+function askPlayerMove(){
+    var move = prompt("Input move: 'r','p', or 's'")
+    console.log(move)
+}
+
+askPlayerMove()
+
+
+
+
+/*while(!gameOver){
+
+}
+*/
+
