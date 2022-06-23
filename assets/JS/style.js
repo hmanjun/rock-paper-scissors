15bc3aa1ddf223aa7c7d5a6d8c7eee2e7440cbd5
hmanjun <harshith.hm03@gmail.com>
2022-06-23 11:54:41 -0700

    Add winner checker + print

--- assets/JS/style.js
+++ assets/JS/style.js
@@ -1,25 +1,51 @@
 var gameOver = false
 var potnArr = ["r","p","s"]
+var playerWins = 0
+var aiWins = 0
 
 
 function askPlayerMove(){
     var move = prompt("Input move: 'r','p', or 's'")
-    return (move)
+    return move
 }
 
 function AIMove(){
     var randint = Math.floor(Math.random()*(3))
-    return (potnArr [randint])
+    return potnArr[randint]
+}
+ //0 means player won, 1 means ai won, 2 means tie
+function checkWinner(pM, aM){
+    if(pM === aM){
+        return 2
+    } else if(pM === 'r' && aM ==='p'){
+        return 1
+    } else if(pM === 'p' && aM ==='s'){
+        return 1
+    } else if(pM === 's' && aM ==='r'){
+        return 1
+    } else {
+        return 0
+    }
+}
+
+function printScore(result){
+    if(result===0){
+        console.log("You won")
+    } else if(result === 1){
+        console.log("AI won")
+    } else {
+        console.log("It's a tie")
+    }
 }
 
 
-askPlayerMove()
-
+var playerMove = askPlayerMove()
+var aiMove = AIMove()
 
 
 
 /*while(!gameOver){
-
+    var result = checkWinner()
 }
 */
 
