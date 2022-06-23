2843d3a5792213d5015aee72da86d24b1a1c5090
Gleepy <107324298+mtwence@users.noreply.github.com>
2022-06-23 14:43:17 -0400

    Add computer move selector

--- assets/JS/style.js
+++ assets/JS/style.js
@@ -1,10 +1,17 @@
 var gameOver = false
 var potnArr = ["r","p","s"]
 
+
 function askPlayerMove(){
     var move = prompt("Input move: 'r','p', or 's'")
-    console.log(move)
+    return (move)
 }
+
+function AIMove(){
+    var randint = Math.floor(Math.random()*(3))
+    return (potnArr [randint])
+}
+
 
 askPlayerMove()
 
